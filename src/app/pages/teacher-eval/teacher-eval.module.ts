c3b22148bf6a10deb949a023df62b47722a17769
jeffymono <jeffymona123@gmail.com>
2020-11-07 10:12:17 -0500

    Answers terminated

--- src/app/pages/teacher-eval/teacher-eval.module.ts
+++ src/app/pages/teacher-eval/teacher-eval.module.ts
@@ -7,11 +7,46 @@ import { QuestionComponent } from './question/question.component';
 import { AnswerComponent } from './answer/answer.component';
 
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
+
+// PrimeNg
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {CardModule} from 'primeng/card';
+import {MessageModule} from 'primeng/message';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from 'primeng/dialog';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ToastModule} from 'primeng/toast';
+import {TableModule} from 'primeng/table';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {TooltipModule} from 'primeng/tooltip';
+import {TabViewModule} from 'primeng/tabview';
+
+
 @NgModule({
   declarations: [EvaluationTypeComponent, QuestionComponent, AnswerComponent],
   imports: [
     CommonModule,
-    TeacherEvalRoutingModule
-  ]
+    TeacherEvalRoutingModule,
+    FormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    CardModule,
+    MessageModule,
+    TranslateModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    DialogModule,
+    ConfirmDialogModule,
+    ToastModule,
+    TableModule,
+    TooltipModule,
+    TabViewModule
+  ],
+  providers: [ConfirmationService, MessageService]
 })
 export class TeacherEvalModule { }
